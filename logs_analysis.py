3c20e98f97644254ae11757d650b4d883da3fba3
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-01 16:45:10 +0000

    Prints paths containing article slug

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,18 +1,26 @@
 """Logs Analysis
 
 Reads three tables:
+
 Table name: articles
     Columns: 
         author INTEGER (F ref. authors(id)), title TEXT, 
         slug TEXT (unique constraint), lead TEXT, body TEXT, 
         time TIMESTAMP, id INTEGER (P)
+
 Table name: authors
     Columns:
         name TEXT, bio TEXT, id INTEGER (P)
+
 Table name: log
     Columns:
         path TEXT, ip INET, method TEXT, status TEXT, 
         time TIMESTAMP, id INTEGER (P)
+
+Answers:
+    What are the most popular articles of all time?
+    Who are the most popular authors of all time?
+    On which days did more than 1% of requests lead to errors?  
 """
 
 import psycopg2
@@ -24,10 +32,14 @@ def main():
     
     conn = psycopg2.connect(database=DB)
     cur = conn.cursor()
-    cur.execute("""SELECT name FROM authors LIMIT 1
+    cur.execute("""SELECT log.path FROM log, articles 
+    WHERE log.path LIKE '%' || articles.slug || '%'
+    GROUP BY log.path
+    LIMIT 10
     """)
     output = cur.fetchall()
-    print(output)
+    for entry in output:
+        print(entry[0])
     conn.close()
 
 
