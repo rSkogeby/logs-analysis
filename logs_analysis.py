05d0f16d4e028a7e1ca2b48e404cb95f37961826
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-01 15:32:01 +0000

    Adds initial cursor operation

--- logs_analysis.py
+++ logs_analysis.py
@@ -8,6 +8,11 @@ DB = 'news'
 def main():
     
     conn = psycopg2.connect(database=DB)
+    cur = conn.cursor()
+    cur.execute("""SELECT name FROM authors LIMIT 1
+    """)
+    output = cur.fetchall()
+    print(output)
     conn.close()
 
 
