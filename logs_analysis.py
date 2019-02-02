56078aa96cfa6797b56134cbc4f28d70634c3450
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-02 09:40:03 +0000

    Reports total number of hits on a particular date.

--- logs_analysis.py
+++ logs_analysis.py
@@ -33,52 +33,62 @@ def main():
     conn = psycopg2.connect(database=DB)
     cur = conn.cursor()
     print()
-    
-    """Reports the most popular articles of all times."""
-    print('Most read articles:')    
-    cur.execute("""SELECT articles.title, COUNT(log.path) as num 
-        FROM log, articles 
-        WHERE log.path LIKE '%' || articles.slug || '%'
-        GROUP BY articles.title
-        ORDER BY num DESC
-        LIMIT 3
-    """)
-    output = cur.fetchall()
-    for entry in output:
-        print('\"%s\"' % entry[0], '--', entry[1], 'views')
-    print()
-    
-    """Reports the most popular author of all times."""
-    print('Most read authors:')
-    cur.execute("""SELECT authors.name, COUNT(log.path) as num
-        FROM log, articles, authors
-        WHERE log.path LIKE '%' || articles.slug || '%'
-        AND articles.author = authors.id
-        GROUP BY authors.name
-        ORDER BY num DESC
-        LIMIT 4
-    """)
-
-    output = cur.fetchall()
-    for entry in output:
-        print('\"%s\"' % entry[0], '--', entry[1], 'views')
-    print()
+    #
+    #"""Reports the most popular articles of all times."""
+    #print('Most read articles:')    
+    #cur.execute("""SELECT articles.title, COUNT(log.path) as num 
+    #    FROM log, articles 
+    #    WHERE log.path LIKE '%' || articles.slug || '%'
+    #    GROUP BY articles.title
+    #    ORDER BY num DESC
+    #    LIMIT 3
+    #""")
+    #output = cur.fetchall()
+    #for entry in output:
+    #    print('\"%s\"' % entry[0], '--', entry[1], 'views')
+    #print()
+    #
+    #"""Reports the most popular author of all times."""
+    #print('Most read authors:')
+    #cur.execute("""SELECT authors.name, COUNT(log.path) as num
+    #    FROM log, articles, authors
+    #    WHERE log.path LIKE '%' || articles.slug || '%'
+    #    AND articles.author = authors.id
+    #    GROUP BY authors.name
+    #    ORDER BY num DESC
+    #    LIMIT 4
+    #""")
+#
+    #output = cur.fetchall()
+    #for entry in output:
+    #    print('\"%s\"' % entry[0], '--', entry[1], 'views')
+    #print()
     
     """Days with more than 1% of requests resulting in errors"""
-    print('Errors:')    
-    cur.execute("""SELECT TO_CHAR(log.time::date, 'FMMonth dd, yyyy') AS day, 
-        COUNT(log.status) AS num
-        FROM log
-        WHERE log.status LIKE '%40%'
-        OR log.status LIKE '%50%'
-        GROUP BY day
-        ORDER BY num DESC
+    print('Errors:')
+  
+    cur.execute("""CREATE VIEW date_and_status AS
+        SELECT TO_CHAR(log.time::date, 'FMMonth dd, yyyy') AS day,
+        log.status as status FROM log;
+     
+     SELECT day, total_num
+        FROM date_and_status, (
+            SELECT COUNT(status) AS total_num
+            FROM date_and_status
+            GROUP BY day
+        ) AS subq
+        WHERE status LIKE '%40%'
+        OR status LIKE '%50%'
+        GROUP BY day, total_num
         LIMIT 10
     """)
 
     output = cur.fetchall()
     for entry in output:
-        print('\"%s\"' % entry[0], '--', entry[1], 'views')
+        for i in entry:
+            print(i)
+        print()
+        #print('\"%s\"' % entry[0], '--', entry[1], 'views')
     
     conn.close()
 
